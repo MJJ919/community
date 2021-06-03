f8b20b9f1f50a04f065780320db0e2bad3a43245
MJJ919 <jmin3@ncsu.edu>
2021-06-03 19:01:48 -0400

    Properties Modification

---
+++ src/main/java/com/example/community/controller/AuthorizeController.java
@@ -0,0 +1,40 @@
+package com.example.community.controller;
+
+import com.example.community.dto.AccessTokenDTO;
+import com.example.community.dto.GithubUser;
+import com.example.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.sql.SQLOutput;
+
+@Controller
+public class AuthorizeController {
+    @Autowired
+    private GithubProvider gitHubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code")String code,
+                           @RequestParam(name = "state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setState(state);
+        String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = gitHubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
